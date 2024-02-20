df66d13c5ba0130ae268aa7e387ec78c7919ce76
Praveen <praveensankar000@gmail.com>
2024-02-20 17:03:10 +0100

    error handle file

--- simulation/simulator.go
+++ simulation/simulator.go
@@ -191,7 +191,10 @@ func  WriteToFile( result Results) {
 	results = append(results, result)
 	jsonRes, _ := json.MarshalIndent(results,"","")
 	//filename := fmt.Sprintf("Simulation/results/result_%v_%v_%v.json",numberOfVcs, numberOfRevokedVcs, mtLevelInDLT)
-	ioutil.WriteFile("Simulation/results.json", jsonRes, 0644)
+	err := ioutil.WriteFile("Simulation/results.json", jsonRes, 0644)
+	if err != nil {
+		zap.S().Errorln("unable to write results to file")
+	}
 
 }
 
