696aaa4e0d49c18b0febf3795433e57fb10df8b3
Praveen <praveensankar000@gmail.com>
2024-04-05 11:04:18 +0200

    updated experiment

--- simulation/calculateFalsePositivesandWitness.go
+++ simulation/calculateFalsePositivesandWitness.go
@@ -175,8 +175,8 @@ func CalculateNumberOfVCsWouldRetrieveWitnessFromDLT(conf config.Config) {
 	expStart := time.Now()
 	//totalVCs:=[]int{10000}
 	//totalVCs:=[]int{50000}
-	totalVCs:=[]int{100000}
-	//totalVCs:=[]int{1000000}
+	//totalVCs:=[]int{100000}
+	totalVCs:=[]int{1000000}
 	processRawData := false
 	revocationPercentages := []int{1,2,3,4,5,6,7,8,9,10,20,30,40}
 
@@ -192,7 +192,7 @@ func CalculateNumberOfVCsWouldRetrieveWitnessFromDLT(conf config.Config) {
 
 
 	rawFilename := fmt.Sprintf("results/results_computed_raw_50000.json")
-	resultFileName := fmt.Sprintf("results/results_computed_50000.json")
+	resultFileName := fmt.Sprintf("results/results_computed_1M.json")
 
 
 	container := Container{}
